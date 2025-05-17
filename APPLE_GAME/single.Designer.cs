11cb45ce456c0bd32993b95e9a936735f42fce5d
KOJAEWON0118 <kojw0108@kw.ac.kr>
2025-05-18 02:53:49 +0900

    Apple_game(single, multi) 추가 겸 테스트

--- APPLE_GAME/single.Designer.cs
+++ APPLE_GAME/single.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace APPLE_GAME
 {
-    partial class Form1
+    partial class single
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
--- APPLE_GAME/single.cs
+++ APPLE_GAME/single.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace APPLE_GAME
 {
-    public partial class Form1 : Form
+    public partial class single : Form
     {
-        public Form1()
+        public single()
         {
             InitializeComponent();
         }
