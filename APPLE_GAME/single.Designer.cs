1e742486eff370327285b9c08018b80d64b2ad0a
KOJAEWON0118 <kojw0108@kw.ac.kr>
2025-05-18 03:47:52 +0900

    progress 추가 밑 테스트

--- APPLE_GAME/single.Designer.cs
+++ APPLE_GAME/single.Designer.cs
@@ -28,21 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.Game_panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.prg_time = new Guna.UI2.WinForms.Guna2VProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtscore = new System.Windows.Forms.TextBox();
+            this.btn_test = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // Form1
+            // Game_panel
+            // 
+            this.Game_panel.Location = new System.Drawing.Point(140, 98);
+            this.Game_panel.Name = "Game_panel";
+            this.Game_panel.Size = new System.Drawing.Size(900, 500);
+            this.Game_panel.TabIndex = 1;
+            // 
+            // prg_time
+            // 
+            this.prg_time.Location = new System.Drawing.Point(1100, 113);
+            this.prg_time.Name = "prg_time";
+            this.prg_time.Size = new System.Drawing.Size(30, 477);
+            this.prg_time.TabIndex = 2;
+            this.prg_time.Text = "guna2VProgressBar1";
+            this.prg_time.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("휴먼둥근헤드라인", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(979, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "점수 : ";
+            // 
+            // txtscore
+            // 
+            this.txtscore.Font = new System.Drawing.Font("휴먼둥근헤드라인", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtscore.Location = new System.Drawing.Point(1062, 49);
+            this.txtscore.Name = "txtscore";
+            this.txtscore.Size = new System.Drawing.Size(80, 34);
+            this.txtscore.TabIndex = 4;
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(28, 12);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(186, 48);
+            this.btn_test.TabIndex = 5;
+            this.btn_test.Text = "테스트용";
+            this.btn_test.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // single
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.btn_test);
+            this.Controls.Add(this.txtscore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prg_time);
+            this.Controls.Add(this.Game_panel);
+            this.Name = "single";
+            this.Text = "apple_game";
+            this.Load += new System.EventHandler(this.single_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.FlowLayoutPanel Game_panel;
+        private Guna.UI2.WinForms.Guna2VProgressBar prg_time;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtscore;
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- APPLE_GAME/single.cs
+++ APPLE_GAME/single.cs
@@ -12,14 +12,38 @@ namespace APPLE_GAME
 {
     public partial class single : Form
     {
+        int remainingTime = 10; //게임 시간 
+        int score = 0; // 테스트용 score
         public single()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void single_Load(object sender, EventArgs e)
         {
-            //test last
+            prg_time.Maximum = remainingTime;
+            prg_time.Value = remainingTime;
+
+            // 타이머 설정
+            timer1.Interval = 1000; // 1초마다          
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (remainingTime > 0)
+            {
+                remainingTime--;
+
+                score++; // 테스트용 점수증가
+                txtscore.Text = score.ToString(); // 테스트용 점수 출력
+                prg_time.Value = remainingTime;
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("시간 종료!");
+            }
         }
     }
 }
